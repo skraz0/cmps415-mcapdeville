a0ff838e0b450c4e396fe70c917fe670c69536e5
Matthew Capdeville <xertot019@gmail.com>
2019-04-27 20:13:28 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+heroku link:
+
+Unfortunately I did wait to the last minute and tried my best to get it done, but in the end here I am at 3:30am. Almost 4 hours past the due time. LOL
+
+Anyways.
+ - I was not able to get the 1 ticket GET request done.
+
+ - Styling looks bland(that's because there is none), but I will definitely add to it before next phase.
+
+ - The JSON data from the POST request overwrites the file when saved and I didn't have time to figure that one out.
---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.locals.tickets = require('./tickets.json');
+
+//GET home page
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+//GET for getting all tickets
+app.get('/rest/list', function(req, res){
+  res.render('all');
+});
+
+//POST for a ticket id search
+app.post('/rest/list', urlencodedParser, function(req, res){
+  var id = req.body.id;
+  res.redirect('/rest/ticket/' + id);
+});
+
+//GET for getting 1 ticket
+app.get('/rest/ticket/:id', function(req, res){
+  res.render('single', {output: req.params.id});
+});
+
+//GET to render the create a new ticket page
+app.get('/rest/ticket', function(req, res){
+  res.render('create');
+});
+
+//POST for creating a new ticket
+app.post('/rest/ticket', urlencodedParser, function(req, res){
+  res.render('ticket-new', {data: req.body});
+
+  fs.writeFile('tickets.json', JSON.stringify(req.body, null, 2), finished);
+  function finished(err){
+    console.log('New Ticket Submitted and Saved.');
+  }
+});
+
+app.listen(3000);
+console.log('Listening on port 3000...');
